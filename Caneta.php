358775454981ee62f9c6745dbc4179983a082937
Diego Schmitz <diego.sctar@gmail.com>
2020-10-20 01:36:43 -0300

    Criado métodos acessores e construtores

--- Caneta.php
+++ Caneta.php
@@ -8,11 +8,79 @@ class Caneta {
     private $ponta;
     protected $carga;
     protected $tampada;
+    
+    //método construtor - exe:01
+    //public function __construct() {
+    //    $this->modelo = "BIC";
+    //    $this->cor = "Verde";
+    //    $this->ponta = 0.8;
+    //    $this->carga = 50;
+    //    $this->tampada = false;
+    //}
+    
+    //metodo construtor - exe:02
+    //public function Caneta() {
+    //    $this->modelo = "BIC";
+    //    $this->cor = "Verde";
+    //    $this->ponta = 0.8;
+    //    $this->carga = 50;
+    //    $this->tampada = false;
+    //}
+    
+    //metodo construtor - exe:03
+    public function Caneta($m, $c, $p, $ca, $t) {
+        $this->modelo = $m;
+        $this->cor = $c;
+        $this->ponta = $p;
+        $this->carga = $ca;
+        $this->tampada = $t;
+    }
 
-    //métodos em php
+    //métodos acessores em php
+    public function getModelo(){
+        return $this->modelo;
+    }
+    
+    public function setModelo($m) {
+        $this->modelo = $m;        
+    }
+    
+    public function getPonta(){
+        return $this->ponta;
+    }
+    
+    public function setPonta($p){
+        $this->ponta = $p;
+    }
+    
+    function getCor() {
+        return $this->cor;
+    }
+
+    function getCarga() {
+        return $this->carga;
+    }
+
+    function getTampada() {
+        return $this->tampada;
+    }
+
+    function setCor($cor) {
+        $this->cor = $cor;
+    }
+
+    function setCarga($carga) {
+        $this->carga = $carga;
+    }
+
+    function setTampada($tampada) {
+        $this->tampada = $tampada;
+    }
+
+        //métodos
     public function rabiscar() {
         if ($this->tampada == true) {
-            echo "<p>Erro não posso rabiscar";
+            echo "<p>Erro! Não posso rabiscar";
         } else {
             echo "<p>Estou rabiscando</p>";
         }
@@ -20,7 +88,6 @@ class Caneta {
 
     public function tampar() {
         $this->tampada = true;
-        
     }
 
     public function destampar() {
--- index.php
+++ index.php
@@ -6,21 +6,23 @@
         <title>Aula02 POO</title>
     </head>
     <body>
-        <?php
-         require 'Caneta.php';
-         
-         // Criando um novo objeto c1 do tipo Caneta, atravez da classe Caneta
-         $c1 = new Caneta();
-         
-         //definindo valor dos atributos
-         $c1->modelo = "Compactor";
-         $c1->cor = "Vermelho";
-         //$c1->ponta = 1.0;
-         //$c1->carga = 60;
-         //$c1->tampada = true;
-         $c1->rabiscar();
-         $c1->destampar();
-         print_r($c1);
-        ?>
+        <pre>
+            <?php
+            require 'Caneta.php';
+
+            // Criando um novo objeto c1 do tipo Caneta, atravez da classe Caneta
+            $c1 = new Caneta("Bic Cristal", "Amarelo", 0.7, 100, true);
+
+            //definindo valor dos atributos
+            //$c1->setModelo("Compactor");
+            //$c1->setPonta(1.2);
+            echo "</br>";
+            print "Eu tenho uma caneta {$c1->getModelo()} de cor {$c1->getCor()} de ponta {$c1->getPonta()} com carga de {$c1->getCarga()}% e tampada {$c1->getTampada()}";
+            
+            $c2 = new Caneta("Stabilo", "Verde", 1.0, 20, false);
+            echo "</br>";
+            print "Eu tenho uma caneta {$c2->getModelo()} de cor {$c2->getCor()} de ponta {$c2->getPonta()} com carga de {$c2->getCarga()}% e tampada {$c2->getTampada()}";
+            ?>
+        </pre>
     </body>
 </html>
